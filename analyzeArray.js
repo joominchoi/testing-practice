218561a340ebc1a3ff2dea173b5b654e010184ff
JooMin Choi <joominchoi23@gmail.com>
2024-01-15 11:20:33 +0000

    Returns and tests min and max numbers of an array

--- analyzeArray.js
+++ analyzeArray.js
@@ -2,8 +2,13 @@ function analyzeArray(arrayOfNumbers) {
   const sum = arrayOfNumbers.reduce((acc, num) => acc + num, 0);
   const average = sum / arrayOfNumbers.length;
 
+  const min = Math.min(...arrayOfNumbers)
+  const max = Math.max(...arrayOfNumbers)
+
   return {
     average: average,
+    min: min,
+    max: max,
     length: arrayOfNumbers.length
   }
 }
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -7,6 +7,18 @@ describe('analyzeArray', () => {
     expect(analysisResult.average).toBe(3);
   });
 
+  test('returns smallest number in array', () => {
+    const analysisResult = analyzeArray([1, 2, 3, 4 ,5]);
+
+    expect(analysisResult.min).toBe(1);
+  })
+
+  test('returns largest number in array', () => {
+    const analysisResult = analyzeArray([1, 2, 3, 4 ,5]);
+
+    expect(analysisResult.max).toBe(5);
+  })
+
   test('returns length of array', () => {
     const analysisResult = analyzeArray([1, 2, 3, 4 ,5]);
 
