7ea27329cf28051cb55ecabca3f7521aad6f26ae
JooMin Choi <joominchoi23@gmail.com>
2024-01-15 11:29:51 +0000

    Add and test returning values for when array is empty

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,7 +1,15 @@
 function analyzeArray(arrayOfNumbers) {
+  if (arrayOfNumbers.length === 0) {
+    return {
+      average: null,
+      min: null,
+      max: null,
+      length: 0,
+    };
+  }
+
   const sum = arrayOfNumbers.reduce((acc, num) => acc + num, 0);
   const average = sum / arrayOfNumbers.length;
-
   const min = Math.min(...arrayOfNumbers)
   const max = Math.max(...arrayOfNumbers)
 
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -8,21 +8,46 @@ describe('analyzeArray', () => {
   });
 
   test('returns smallest number in array', () => {
-    const analysisResult = analyzeArray([1, 2, 3, 4 ,5]);
+    const analysisResult = analyzeArray([1, 2, 3, 4, 5]);
 
     expect(analysisResult.min).toBe(1);
-  })
+  });
 
   test('returns largest number in array', () => {
-    const analysisResult = analyzeArray([1, 2, 3, 4 ,5]);
+    const analysisResult = analyzeArray([1, 2, 3, 4, 5]);
 
     expect(analysisResult.max).toBe(5);
-  })
+  });
 
   test('returns length of array', () => {
-    const analysisResult = analyzeArray([1, 2, 3, 4 ,5]);
+    const analysisResult = analyzeArray([1, 2, 3, 4, 5]);
 
     expect(analysisResult.length).toBe(5);
-  })
+  });
 });
 
+describe('Empty array', () => {
+  test('average returns null', () => {
+    const analysisResult = analyzeArray([]);
+
+    expect(analysisResult.average).toBe(null);
+  });
+
+  test('min returns null', () => {
+    const analysisResult = analyzeArray([]);
+
+    expect(analysisResult.min).toBe(null);
+  });
+
+  test('max returns null', () => {
+    const analysisResult = analyzeArray([]);
+
+    expect(analysisResult.max).toBe(null);
+  });
+
+  test('length returns 0', () => {
+    const analysisResult = analyzeArray([]);
+
+    expect(analysisResult.length).toBe(0);
+  });
+});
